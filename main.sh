a07ee7d5ca9dce19a76834900330555bc087779d
sistemaslabelgrup <62298148+sistemaslabelgrup@users.noreply.github.com>
2023-01-02 12:54:20 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,7 +10,10 @@ echo "Install helm"
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
 echo "Install GCP CLI"
-RUN echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && apt-get update -y && apt-get install google-cloud-cli -y
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && apt-get update -y && apt-get install google-cloud-cli -y
+gcloud components install gke-gcloud-auth-plugin
+gke-gcloud-auth-plugin --version
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 
 echo "Install kubent"
 sh -c "$(curl -sSL https://git.io/install-kubent)"
